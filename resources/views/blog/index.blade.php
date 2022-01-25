d5b31d29d01d571f9d4ac787e87d8c41ec61752f
unknown <fotoprueba8@gmail.com>
2022-01-25 01:24:04 -0600

    carga

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet" />
+    <link href="fontawesome/css/all.min.css" rel="stylesheet" />
+    <link href="css/templatemo-comparto.css" rel="stylesheet" />
+    <title>Comparto HTML CSS Template</title>
+
+</head>
+
+<body>
+    <!-- Back to top button -->
+    <a id="button"><i class="fas fa-angle-up fa-2x"></i></a>
+    <div class="container-fluid">
+        <div class="tm-site-header tm-mb-1">
+            <div class="tm-site-name-container tm-bg-color-1">
+                <h1 class="tm-text-white">Caje-Churro</h1>
+            </div>
+            <div class="tm-nav-container tm-bg-color-8">
+                <nav class="tm-nav" id="tm-nav">
+                    <ul>
+                        <li class="tm-nav-item current">
+                            <a href="#about" class="tm-nav-link">
+                                <span class="tm-mb-1">.01</span>
+                                <span>Bienvenida</span>
+                            </a>
+                        </li>
+                        <li class="tm-nav-item">
+                            <a href="#services" class="tm-nav-link">
+                                <span class="tm-mb-1">.02</span>
+                                <span>¿De que trata?</span>
+                            </a>
+                        </li>
+                        <li class="tm-nav-item">
+                            <a href="#gallery" class="tm-nav-link">
+                                <span class="tm-mb-1">.03</span>
+                                <span>Mis trabajos</span>
+                            </a>
+                        </li>
+                        <li class="tm-nav-item">
+                            <a href="#contact" class="tm-nav-link">
+                                <span class="tm-nav-text tm-mb-1">.04</span>
+                                <span class="tm-nav-text">Redes sociales</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <section class="tm-mb-1" id="about">
+            <div class="tm-row tm-about-row">
+                <div class="tm-section-1-l">
+                    <img src="img/icon.jpg" alt="About image" class="tm-img-responsive">
+                </div>
+                <article class="tm-section-1-r tm-bg-color-8">
+                    <h2 class="tm-mb-2 tm-title-color">.01 Bienvenidos!</h2>
+                    <p><a rel="nofollow" href="https://templatemo.com/tm-544-comparto" target="_parent">¡Hola!</a> ¡Bienvenidos a mi blog de arte!.</p>
+                    <p>Mi nombre es Caje, actualmente tengo 19 años, el dibujo es mi gran pasion y aparte disfruto mucho compartir mi arte al igual de crear contenido a base de mis dibujos! <a rel="nofollow" href="https://templatemo.com/contact" target="_parent"></a> </p>
+                    <p>Realmente espero que se sientan bienvenidos a este proyecto al cual llamo "Caje Churro".</p>
+                    
+                    <a href="#services" class="tm-btn tm-btn-1 tm-link-to-services">¿De que trata?</a>
+                </article>
+            </div>
+        </section>
+        <div class="tm-bg-color-1 tm-mb-1 tm-row tm-social-row">
+            <div class="tm-icon">
+                <div class="tm-icon-inner">
+                    
+                </div>
+            </div>
+            <div class="tm-icon">
+                <div class="tm-icon-inner">
+                    <a href="#gallery">
+                        <i class="fas fa-chart-bar fa-4x tm-mb-1"></i> <a href="https://www.instagram.com/p/CV80xdGJBFJ/">
+                        <p>Mis comisiones</p>
+                        
+                    </a>
+                </div>
+            </div>
+           
+        </div>
+        <section class="tm-mb-1 tm-row tm-services-row" id="services">
+            <div class="tm-section-2-l">
+                <article class="tm-bg-color-6 tm-box-pad tm-mb-1">
+                    <h2 class="tm-mb-2">.02 ¿De que trata?</h2>
+                    <p>Caje Churro es un proyecto que e estado trabajando en estos ultimos años, en cual tiene el proposito de compartir mi proceso de dibujo en estos años por mediante de redes sociales </p>
+                    <p class="tm-mb-0">Al igual de crear contenido para youtube mediante videos narrados, tutoriales, animatics y pequeñas animaciones.</p>
+                </article>
+                <div class="tm-bg-color-7 tm-em-box">
+                    <p class="tm-text-color-2">Igualmente el proyecto tiene como proposito principal igualmente compartir el proceso de mis personajes, ya que en estos años e estado trabajando en desarrollar una historia para que este año 2022 subir un futuro Webcomic.</p>
+                    <a href="#gallery" class="tm-btn tm-btn-2">Mis trabajos</a>
+                </div>
+            </div>
+            <div class="tm-section-2-r">
+                <img src="img/my melody firma.jpg" alt="Services image" class="tm-img-responsive">
+            </div>
+        </section>
+        <section class="tm-bg-color-4 tm-mb-3 tm-gallery-section" id="gallery">
+            <div class="tm-gallery-header">
+                <h2 class="tm-mb-1 text-right">.03 Mis trabajos</h2>
+                <ul class="tm-gallery-filter tabs clearfix filters-button-group">
+                   
+                </ul>
+            </div>
+            <div class="tm-gallery-outer">
+                <div class="tm-gallery" id="tm-gallery">
+                    <div class="tm-gallery-item nature">
+                        <figure class="effect-bubba">
+                            <img src="img/gallery/regalo 2.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            <figcaption>
+                                
+                                
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="tm-gallery-item animals">
+                        <figure class="effect-bubba">
+                            <img src="img/gallery/caje gamer rosa.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            <figcaption>
+                            
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="tm-gallery-item nature">
+                        <figure class="effect-bubba">
+                            <img src="img/gallery/ranita pixelart.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            
+                        </figure>
+                    </div>
+                    <div class="tm-gallery-item nature">
+                        <figure class="effect-bubba">
+                            <img src="img/gallery/mini caje pato sin fondo.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            
+                        </figure>
+                    </div>
+                    <div class="tm-gallery-item animals">
+                        <figure class="effect-bubba">
+                            <img src="img/gallery/caje encuesta.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            
+                        </figure>
+                    </div>
+                    <div class="tm-gallery-item animals">
+                        <figure class="effect-bubba">
+                            <img src="img/clip studio.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            
+                        </figure>
+                    </div>
+                    <div class="tm-gallery-item peopl">
+                        <figure class="effect-bubba">
+                            <img src="img/gallery/diabla redraw hermana.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            <figcaption>
+                                
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="tm-gallery-item people">
+                        <figure class="effect-bubba">
+                            <img src="img/gallery/GAEL castaño.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            <figcaption>
+                                
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="tm-gallery-item people">
+                        <figure class="effect-bubba">
+                            <img src="img/gallery/regalo 1.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            <figcaption>
+                                
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="tm-gallery-item people">
+                        <figure class="effect-bubba">
+                            <img src="img/gallery/durazno a.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            <figcaption>
+                                <h2>Share <span>This</span></h2>
+                                <p>You can make your own image gallery.</p>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </section> <!-- Gallery -->
+        <section id="contact" class="tm-bg-color-5 tm-mb-3">
+            <h2 class="tm-text-white tm-contact-title">.04 Mis redes sociales</h2>
+            
+            <div class="tm-bg-color-white tm-contact-main">
+                <div class="map-outer">
+                    <div class="gmap-canvas">
+                    
+                        
+                    </div>
+                </div>
+                <div class="contact-form-outer">
+                <div class="tm-mb-4 text-center tm-social-s">
+            <a href="https://www.facebook.com/CajetaChurro" class="tm-social-link"><i class="fab fa-facebook tm-social-icon"></i></a>
+            <a href="https://www.instagram.com/caje_churro/?hl=es-la" class="tm-social-link"><i class="fab fa-instagram tm-social-icon"></i></a>
+            <a href="https://twitter.com/cajetachurro1" class="tm-social-link"><i class="fab fa-twitter tm-social-icon"></i></a>
+            <a href="https://www.youtube.com/channel/UCLkU-P9WcS3LeNYtKX0dkGQ" class="tm-social-link"><i class="fab fa-youtube tm-social-icon"></i></a>
+                </div>
+                
+            </div>
+        </section>
+        
+        </div>
+        
+    </div> <!-- .container -->
+    <script src="js/jquery.min.js"></script> <!-- https://jquery.com/download/ -->
+    <script src="js/imagesloaded.pkgd.min.js"></script> <!-- https://imagesloaded.desandro.com/ -->
+    <script src="js/isotope.pkgd.min.js"></script> <!-- https://isotope.metafizzy.co/ -->
+    <script src="js/jquery.singlePageNav.min.js"></script> <!-- https://github.com/ChrisWojcik/single-page-nav -->
+    <script>
+
+        // Scroll to Top button
+        var btn = $('#button');
+
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > 300) {
+                btn.addClass('show');
+            } else {
+                btn.removeClass('show');
+            }
+        });
+
+        btn.on('click', function (e) {
+            e.preventDefault();
+            $('html, body').animate({ scrollTop: 0 }, '300');
+        });
+
+        // DOM is ready
+        $(function () {
+            // Single Page Nav
+            $('#tm-nav').singlePageNav({ speed: 600 });
+
+            // Smooth Scroll (https://css-tricks.com/snippets/jquery/smooth-scrolling/)
+            $('a[href*="#"]')
+                // Remove links that don't actually link to anything
+                .not('[href="#"]')
+                .not('[href="#0"]')
+                .click(function (event) {
+                    // On-page links
+                    if (
+                        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                        &&
+                        location.hostname == this.hostname
+                    ) {
+                        // Figure out element to scroll to
+                        var target = $(this.hash);
+                        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                        // Does a scroll target exist?
+                        if (target.length) {
+                            // Only prevent default if animation is actually gonna happen
+                            event.preventDefault();
+                            $('html, body').animate({
+                                scrollTop: target.offset().top
+                            }, 600, function () {
+                                // Callback after animation
+                                // Must change focus!
+                                var $target = $(target);
+                                $target.focus();
+                                if ($target.is(":focus")) { // Checking if the target was focused
+                                    return false;
+                                } else {
+                                    $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                                    $target.focus(); // Set focus again
+                                };
+                            });
+                        }
+                    }
+                });
+
+            /* Isotope Gallery */
+
+            // init isotope
+            var $gallery = $(".tm-gallery").isotope({
+                itemSelector: ".tm-gallery-item",
+                layoutMode: "fitRows"
+            });
+            // layout Isotope after each image loads
+            $gallery.imagesLoaded().progress(function () {
+                $gallery.isotope("layout");
+            });
+
+            $(".filters-button-group").on("click", "a", function () {
+                var filterValue = $(this).attr("data-filter");
+                $gallery.isotope({ filter: filterValue });
+            });
+
+            $(".tabgroup > div").hide();
+            $(".tabgroup > div:first-of-type").show();
+            $(".tabs a").click(function (e) {
+                e.preventDefault();
+                var $this = $(this),
+                    tabgroup = "#" + $this.parents(".tabs").data("tabgroup"),
+                    others = $this
+                        .closest("li")
+                        .siblings()
+                        .children("a"),
+                    target = $this.attr("href");
+                others.removeClass("active");
+                $this.addClass("active");
+            });
+        });
+    </script>
+</body>
+</html>
